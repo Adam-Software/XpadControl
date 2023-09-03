5c100f516ff7ce33f54f202f4d97e028d6771c71
Alexey Nesterov <vertigra.developer@gmail.com>
2023-09-03 20:05:37 +1000

    Added: info about connected devices

--- XpadControl.Windows.Services/GamepadService/GamepadService.cs
+++ XpadControl.Windows.Services/GamepadService/GamepadService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using XInputium.XInput;
 using XpadControl.Interfaces.GamepadService;
@@ -40,6 +41,9 @@ namespace XpadControl.Windows.Services.GamepadService
             }
 
             Task.Run(Start);
+
+            mLoggerService.WriteVerboseLog($"Is gamepad connected: {mGamepad.IsConnected}");
+
         }
         private async Task Start()
         {

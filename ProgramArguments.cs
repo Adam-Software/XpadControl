eb2155d3916dd3985b397c7cc9b7a4cb5349ffe3
Alexey Nesterov <vertigra.developer@gmail.com>
2023-09-13 07:51:23 +1000

    Fix: scopes for argument class

--- ProgramArguments.cs
+++ ProgramArguments.cs
@@ -2,24 +2,24 @@
 
 namespace XpadControl
 {
-    internal class ProgramArguments
+    public class ProgramArguments
     {
-        internal const string DefaultConfigName = $"appsettings.default.json";
+        private const string DefaultConfigName = $"appsettings.default.json";
 
         [ValueArgument(typeof(string), 'c', 
             LongName = "config", 
             Description = "Set the path to the settings file",
             DefaultValue = DefaultConfigName,
             ValueOptional = true)]
-        internal string ConfigPathName;
+        public string ConfigPathName;
 
         [SwitchArgument('v',  "version", 
             defaultValue: false,
             Description = "Show app version")]
-        internal bool ShowVersion; 
+        public bool ShowVersion; 
 
         [SwitchArgument('h', "help", defaultValue: false, 
             Description = "Show this help")]
-        internal bool ShowHelp;
+        public bool ShowHelp;
     }
 }

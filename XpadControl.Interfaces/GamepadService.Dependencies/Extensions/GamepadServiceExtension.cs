46ac0f2eb36dc47a04b7a20e1f35d555b285882f
Alexey Nesterov <vertigra.developer@gmail.com>
2023-09-13 12:22:15 +1000

    Update extension and test

--- XpadControl.Interfaces/GamepadService.Dependencies/Extensions/GamepadServiceExtension.cs
+++ XpadControl.Interfaces/GamepadService.Dependencies/Extensions/GamepadServiceExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace XpadControl.Interfaces.GamepadService.Dependencies.Extensions
+﻿using System;
+
+namespace XpadControl.Interfaces.GamepadService.Dependencies.Extensions
 {
     public static class GamepadServiceExtension
     {
@@ -7,13 +9,55 @@
         /// </summary>
         public static float ThumbToFloat(this short axis)
         {
-            return ((float)axis) / (axis >= 0 ? 32767 : 32768);
+            float floatAxis = ((float)axis) / (axis >= 0 ? 32767 : 32768);
+            return Clamp11(floatAxis);
         }
 
         public static float TriggerToFloat(this short trigger)
         {
-            ushort usingedTrigger = (ushort)(trigger + 32768);
-            return ((float)usingedTrigger) / 65535;
+            //need calibrate or deadzone
+            if (trigger == -32767)
+                trigger = -32768;
+
+            ushort usingedTrigger = (ushort) (trigger + 32768);
+            var floatTrigger = ((float)usingedTrigger) / 65535;
+            return Clamp11(floatTrigger);
+        }
+
+        /// <summary>
+        /// Copy from <see cref="https://github.com/AderitoSilva/XInputium/blob/main/source/XInputium/XInputium/InputMath.cs
+        /// 
+        /// Truncates the specified <see cref="float"/> value to the -1 to 1 
+        /// inclusive range.
+        /// </summary>
+        /// <param name="value">A <see cref="float"/> value to truncate.</param>
+        /// <returns>-1 if <paramref name="value"/> is lower than or equal to -1, 
+        /// 1 if <paramref name="value"/> is greater than or equal to 1, 
+        /// or <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="value"/> is <see cref="float.NaN"/>.</exception>
+        public static float Clamp11(float value)
+        {
+            if (float.IsNaN(value))
+                throw new ArgumentException(
+                    $"'{float.NaN}' is not a valid value for '{nameof(value)}' parameter.",
+                    nameof(value));
+
+            if (value < -1f)
+            {
+                return (float)-1f;
+            }
+            else
+            {
+                if (value > 1f)
+                {
+                    return (float)((float)1f);
+                }
+                else
+                {
+                    return (float)((float)value);
+                }
+            }
         }
     }
 }
--- XpadControl.Math.Tests/GamepadServiceExtensionTest.cs
+++ XpadControl.Math.Tests/GamepadServiceExtensionTest.cs
@@ -10,21 +10,41 @@ namespace XpadControl.Math.Tests
         [Test]
         public void TestConvertsThumbToFloat()
         {
-            short minAxis = -32768;
-            short maxAxis = 32767;
+            short minAxis = short.MinValue;
+            short maxAxis = short.MaxValue;
 
-            Assert.That(minAxis.ThumbToFloat(), Is.EqualTo(-1));
-            Assert.That(maxAxis.ThumbToFloat(), Is.EqualTo(1));
+            Assert.Multiple(() =>
+            {
+                Assert.That(minAxis.ThumbToFloat(), Is.EqualTo(-1));
+                Assert.That(maxAxis.ThumbToFloat(), Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void TestConvertsThumbRange()
+        {
+            short minAxis = short.MinValue;
+            short maxAxis = short.MaxValue;
+
+            for(short i = minAxis; i<= maxAxis; i++)
+            {
+                Assert.That(i.ThumbToFloat(), Is.InRange(-1f, 1f));
+            }
         }
 
         [Test]  
         public void TestConvertsTriggerToFloat()
         {
             short minTrigger = short.MinValue;
+            short deadZoneValue = -32767;
             short maxTrigger = short.MaxValue;
-
-            Assert.That(minTrigger.TriggerToFloat(), Is.EqualTo(0));
-            Assert.That(maxTrigger.TriggerToFloat(), Is.EqualTo(1));
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(minTrigger.TriggerToFloat(), Is.EqualTo(0));
+                Assert.That(deadZoneValue.TriggerToFloat(), Is.EqualTo(0));
+                Assert.That(maxTrigger.TriggerToFloat(), Is.EqualTo(1));
+            });
         }
     }
 }

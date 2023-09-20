738c47d083ee14ec101f4e636300bf8177ba2f99
Alexey Nesterov <vertigra.developer@gmail.com>
2023-09-21 09:54:23 +1000

    Added: servo model

---
+++ XpadControl.Interfaces/WebSocketClientsService.Dependencies/JsonModel/ServoCommands.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace XpadControl.Interfaces.WebSocketClientsService.Dependencies.JsonModel
+{
+    public class ServoCommands
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("goal_position")]
+        public int GoalPosition { get; set; }
+
+    }
+}
---
+++ XpadControl.Interfaces/WebSocketClientsService.Dependencies/JsonModel/ServoCommandsItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace XpadControl.Interfaces.WebSocketClientsService.Dependencies.JsonModel
+{
+    public class ServoCommandsItem
+    {
+        [JsonPropertyName("motors")]
+        public List<ServoCommands> Motors { get; set; }
+    }
+}

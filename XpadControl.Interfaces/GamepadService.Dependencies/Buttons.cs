3f3d911dd93f907ded7dc62767dfb15f38d2cfd0
Alexey Nesterov <vertigra.developer@gmail.com>
2023-09-22 12:32:57 +1000

    Fix issue #17

--- XpadControl.Interfaces/GamepadService.Dependencies/Buttons.cs
+++ XpadControl.Interfaces/GamepadService.Dependencies/Buttons.cs
@@ -9,49 +9,49 @@
     {
 
         // No button. This is used to represent no buttons.
-        None,
+        None = 0x0,
 
         // D-Pad Up. This is one of the directional buttons.
-        DPadUp,
+        DPadUp = 0x1,
 
 
         // D-Pad Down. This is one of the directional buttons.
-        DPadDown,
-        
+        DPadDown = 0x2,
+
         // D-Pad Left. This is one of the directional buttons.
-        DPadLeft,
-        
+        DPadLeft = 0x4,
+
         // D-Pad Right. This is one of the directional buttons.
-        DPadRight,
-        
+        DPadRight = 0x8,
+
         // The Start button.
-        Start,
-        
+        Start = 0x10,
+
         // The Back button.
-        Back,
-        
+        Back = 0x20,
+
         // The LS (Left Stick) button.
-        LS,
-        
+        LS = 0x40,
+
         // The RS (Right Stick) button.
-        RS,
-        
+        RS = 0x80,
+
         // The LB (Left Shoulder) button.
-        LB,
-        
+        LB = 0x100,
+
         // The RB (Right Shoulder).
-        RB,
-        
+        RB = 0x200,
+
         // The A button.
-        A,
-        
+        A = 0x1000,
+
         // The B button.
-        B,
-        
+        B = 0x2000,
+
         // The X button.
-        X,
-        
+        X = 0x4000,
+
         // The Y button.
-        Y
+        Y = 0x8000
     }
 }
